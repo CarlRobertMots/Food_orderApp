3ff23008bd1048e8388cdacf704c09adab02b0cc
Carl-Robert <robertm6ts@gmail.com>
2025-04-01 11:12:21 +0300

    Yl5 Valuuta andmete kuvamine ja css tekst edit

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -1,13 +1,16 @@
 const MealItem = ({ meal }) => {
+    const valuuta = new Intl.NumberFormat('et-EE',{
+        style: 'currency',
+        currency: 'EUR',
+    });
     return (
         <li className="meal-item">
             <article>
-                {/* Pildi kuvamine otse JSON-failist saadud teega */}
                 <img src={meal.image} alt={meal.name} />
 
                 <div>
                     <h3>{meal.name}</h3>
-                    <p className="meal-price">€{parseFloat(meal.price).toFixed(2)}</p>
+                    <p className="meal-price">{valuuta.format(meal.price)}</p>
                     <p className="meal-description">{meal.description}</p>
                 </div>
 
