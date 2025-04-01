851be507fda0cb063b798bf29d79484ec89b4779
Carl-Robert <robertm6ts@gmail.com>
2025-04-01 09:36:59 +0300

    Yl 1 Header ja Meals komponentide lisamine rakendusele

--- backend/index.js
+++ backend/index.js
@@ -17,8 +17,12 @@ app.use((req, res, next) => {
 });
 
 app.get("/meals", async (req, res) => {
+  const filePath = path.join(__dirname, "data", "meals.json");
   const meals = "[]" // data should be read from file
+
   res.json(JSON.parse(meals));
+  console.error("Error reading meals file:", error);
+  res.status(500).json({ error: "Failed to read meals data" });
 });
 
 app.use((req, res) => {
